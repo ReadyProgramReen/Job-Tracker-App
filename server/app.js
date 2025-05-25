17c1fb81546dd38c3910fd9be60e6738e9b29553
Maureen E <maureenekeugo1@gmail.com>
2025-05-25 16:07:33 -0400

    enabled CORS to allow frontend to access backend

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 import dotenv from "dotenv"
 dotenv.config(); //needs to be before the express 
+
 import express from "express"
+import cors from "cors"
+
 import {jobsRouter}  from "./routes/jobs.js"
 
 const app = express()
+
+//cross origin resource sharing 
+app.use(cors())
+//parse req.body data 
 app.use(express.json());
 
 
