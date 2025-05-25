b82f9a205e3d23db6b444f32edf04f1fc8b9a13b
Maureen E <maureenekeugo1@gmail.com>
2025-05-25 13:10:50 -0400

    installed Uuid and implemented PUT /jobs/:id to update job status

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -1,4 +1,6 @@
 import express from "express"
+import { v4 as uuidv4 } from 'uuid';
+
 
 export const jobsRouter = express.Router()
 
@@ -15,13 +17,38 @@ jobsRouter.get('/',(req,res)=>{
 jobsRouter.post('/',(req,res)=>{  
 // Access the job data sent in the request body
     const newJobs = req.body
-    console.log(newJobs)  
+    console.log(newJobs) 
+//updated each object to include a unique id
+    const jobWithId = { id: uuidv4(), ...newJobs }; 
 //saved the new jobs by adding it into the jobs array 
-    jobs.push(newJobs)
+    jobs.push(jobWithId)
 //Respond with a 201 status code (Created) and sent back a message confirming the data was added 
     res.status(201).json({
         message:"Job added Successfully",
-        job:newJobs
+        job:jobWithId
     })
+})
+
+//Update the jobs array 
+jobsRouter.put('/:id', (req,res)=>{
+// get the jobs id from the request paramter
+    const usersJobId = req.params.id;
+//find the exact id in the job array
+let foundJob = jobs.find(job=> job.id === usersJobId)
+
+console.log("Users id input",usersJobId)
+console.log("Found id",foundJob)
+
+//see what new/updated data the user/client is sending through the body
+const updatedJobData = req.body;
+
+//update the job database with new updated data from the req.body
+ foundJob.status = updatedJobData.status
+
+
+res.status(200).send('id found')
+
+
+
 })
 
